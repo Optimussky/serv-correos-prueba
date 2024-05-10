928c0703af8a9a19c1f0fdedf5a9539883075899
Alberto Romero <optimussky05@gmail.com>
2024-05-09 19:56:28 -0600

    Añadiendo modelos, vistas,urls, serializers, manejo de autenticación con JWT

--- login/api/auth/views.py
+++ login/api/auth/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from login.models import Login
 from login.api.serializer import LoginSerializer
 
 class LoginViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Login.objects.all()
     serializer_class = LoginSerializer
